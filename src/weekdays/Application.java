d49cc245d6fd46115df3dd54f1f2a6ff520360bb
Ville Niemi <vill3ni3mi@gmail.com>
2018-10-16 17:23:54 +0300

    need to simplify moving freedays

--- src/weekdays/Application.java
+++ src/weekdays/Application.java
@@ -26,17 +26,19 @@ public class Application {
         for(int i = 0; i < 14; i++){
             for(int j = 0; j < 10; j++){
                 total += IntStream.of(matrix[j][i]).sum();
-            }
+            }          
             if(total >= max){
                 max = total;
                 column = i;
             }
             total = 0;
         }
+        System.out.println(column);
         return column;
     }
     
     public int ColumnMin(int[][] matrix){
+        List<Integer> list = new ArrayList();
         min = 5;
         int total = 0;
         int column = 0;
@@ -45,110 +47,63 @@ public class Application {
             for(int j = 0; j < 10; j++){
                 total += IntStream.of(matrix[j][i]).sum();
             }
-            if(total <= min){
-                min = total;
+            if(i == 5 || i == 6 || i == 12 || i == 13){
+                list.add(total - 5);
+            }
+            else{
+                list.add(total - 2);
+            }
+            if(list.get(i) <= min){
+                min = list.get(i);
                 column = i;
             }
             total = 0;
         }
+        System.out.println(column);
         return column;
     }
     
-    public int[][] ColumnMove(int[][] matrix, int column1, int column2){
+    public int ColumnSum(int[][] matrix, int column){
+        int sum = 0;
+        
         for(int i = 0; i < 10; i++){
-            //int i = randomNum(0, 9);
-            if(matrix[i][column1] == 1){
-                matrix[i][column1] = 0;
-                matrix[i][column2] = 1;
+            sum += IntStream.of(matrix[i][column]).sum();
+        }
+        System.out.println(sum);
+        return sum;
+    }
+       
+    public int[][] MoveV(int cMax, int cMin, int[][] matrix){
+        int sum = ColumnSum(matrix, cMin);
+        
+        for(int i = 0; i < 10; i++){
+            if(matrix[i][cMax] == 1){
+                matrix[i][cMax] = 0;
+                if((cMin == 5 || cMin ==6 || cMin == 12 || cMin == 13) && sum - 5 != 0){
+                    matrix[i][cMin] = 1;
+                } else if (ColumnSum(matrix, cMin) - 2 != 0 && (cMin == 0||cMin ==1||cMin ==2||cMin ==3||cMin ==4||cMin ==7||cMin ==8||cMin ==9||cMin ==10||cMin ==11)){
+                    matrix[i][cMin] = 1;
+                }
                 break;
             }
         }
+        
         return matrix;
     }
     
     public void Solve(int[][] matrix){
-        int maxColumn = 0;
-        int minColumn = 0;
-        int turns = 0;
-        int check = ColumnCheck(matrix);
+        int turn = 0;
         
-        while(check > 2){
-            maxColumn = ColumnMax(matrix);
-            minColumn = ColumnMin(matrix);
+        while(turn < 30){
+            int cMax = ColumnMax(matrix);
+            int cMin = ColumnMin(matrix);
         
-            matrix = ColumnMove(matrix, maxColumn, minColumn);
-            check = ColumnCheck(matrix);
-            turns += 1;
-            System.out.println(turns);
-            if(turns > 100){
-                break;
-            }
+            matrix = MoveV(cMax, cMin, matrix);
+            turn += 1;
         }
-            
-        for(int i = 0; i < matrix.length; i++){
-            System.out.println(Arrays.toString(matrix[i]));          
+        
+        for (int[] matrix1 : matrix) {
+            System.out.println(Arrays.toString(matrix1));          
         }
     }
-    
-    public int ColumnCheck(int[][] matrix){
-        int total = 0;
-        List list = new ArrayList();
-        for(int i = 0; i < 14; i++){
-            for(int j = 0; j < 10; j++){
-                total += IntStream.of(matrix[j][i]).sum();
-            }
-            list.add(total);
-        }
-        return (int) Collections.max(list);
-    }
-//    public int[][] RowDays(int [][] matrix){
-//        int total = 0;
-//        for(int i = 0; i < 10; i++){
-//                for(int j = 0; j < 14; j++){
-//                    total += IntStream.of(matrix[i][j]).sum();
-//                }
-//                if(total < 4){
-//                    
-//                    break;
-//                }
-//                
-//        return matrix;
-//    }
-//    public int[][] RowCheck(int column, int[][] matrix){
-//        int total = 0;
-//        for(int i = 0; i < 10; i++){
-//            for(int j = 0; j < 14; j++){
-//                total += IntStream.of(matrix[i][j]).sum();
-//            }
-//            if(total > 4){
-//                matrix[i][column] = 0;               
-//            }
-//        }
-//        return matrix;
-//    }
-    
-//    public int[][] Solve(int max, int[][] matrix){
-//        int total = 0;
-//        for(int i = 0; i < 14; i++){
-//                for(int j = 0; j < 10; j++){
-//                    total += IntStream.of(matrix[j][i]).sum();
-//                }
-//                if(total == max){
-//                    total = 0;
-//                    for(int k = 0; k < 10; k++){
-//                        for(int l = 0; l < 14; l++){
-//                            total += IntStream.of(matrix[k][l]).sum();
-//                        }
-//                        if(total > 4){
-//                            matrix[k][i] = 0;
-//                            break;
-//                        }
-//                    }
-//                    break;
-//                }
-//                total = 0;
-//            }
-//        
-//        return matrix;
-//    }
 }
