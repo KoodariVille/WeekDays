238aeb8ae316cefc67492d28d5e78a2b557cef3f
Ville Niemi <vill3ni3mi@gmail.com>
2018-10-17 17:34:04 +0300

    basic alghorithm completed

--- src/weekdays/Application.java
+++ src/weekdays/Application.java
@@ -17,22 +17,32 @@ import java.util.Collections;
  */
 public class Application { 
     public int ColumnMax(int[][] matrix){
+        List<Integer> list = new ArrayList();
         int max = 0;
         int total = 0;
         int column = 0;
         
         for(int i = 0; i < 14; i++){
             total += ColumnSum(matrix,i);
-            if(total > max){
-                max = total;
-                column = i;
+            if(i == 5 || i == 6 ||i == 12 || i == 13){
+                list.add(total - 5);
+                if(list.get(i) >= max){
+                    max = list.get(i);
+                    column = i;
+                }
+            }else{
+                list.add(total - 2);
+                if(list.get(i) >= max){
+                    max = list.get(i);
+                    column = i;
+                }
             }
             total = 0;
         }
         
-        System.out.println("max" + column);
+//        System.out.println("max " + column);
         return column;
-    }
+    }   
     
     public int ColumnMin(int[][] matrix){
         List<Integer> list = new ArrayList();
@@ -57,8 +67,36 @@ public class Application {
             }
             total = 0;
         }
-        System.out.println("min" + column);
+//        System.out.println("min " + column);
         return column;
+    }
+    
+    public int ColumnMin(int[][] matrix, int iMin){
+        List<Integer> list = new ArrayList();
+        List<Integer> list2 = new ArrayList();
+        int total = 0;
+        int min = matrix[0][0];
+        
+        for(int i = 0; i < 14; i++){
+            total += ColumnSum(matrix,i);
+            if(i == 5 || i == 6 ||i == 12 || i == 13){
+                list.add(total - 5);
+                if(list.get(i) <= min){
+                    min = list.get(i);
+                    list2.add(i);
+                }
+            }else{
+                list.add(total - 2);
+                if(list.get(i) <= min){
+                    min = list.get(i);
+                    list2.add(i);
+                }
+            }
+            total = 0;
+        }
+        int e = list2.get(list2.size() - iMin);
+//        System.out.println("min2 " + 3);
+        return e;
     }
     
     public int ColumnSum(int[][] matrix, int column){
@@ -67,42 +105,69 @@ public class Application {
         for(int i = 0; i < 10; i++){
             sum += IntStream.of(matrix[i][column]).sum();
         }
-        
-        System.out.print(sum);
+
         return sum;
     }
        
     public int[][] MoveV(int cMax, int cMin, int[][] matrix){
-        
         for(int i = 0; i < 10; i++){
             if(matrix[i][cMax] == 1){
                 matrix[i][cMax] = 0;
                 if(matrix[i][cMin] == 0){
                     matrix[i][cMin] = 1;
-                    break;
                 }else{
-                    
+                    for(int j = 2; j <= 4; j++){
+                        cMin = ColumnMin(matrix,j);
+                        if(matrix[i][cMin] == 0){
+                            matrix[i][cMin] = 1;
+                            break;
+                        }
+                    }
                 }
                 break;
             }
-        }        
+        }
+           
         return matrix;
     }
     
     public void Solve(int[][] matrix){
-        int turn = 0;
-        while(true){
-            int cMax = ColumnMax(matrix);
-            int cMin = ColumnMin(matrix);
+        int vSum = VacSum(matrix);
+        int MaxColumn, MinColumn;
+                       
+        while(vSum != 0){
+            MaxColumn = ColumnMax(matrix);
+            MinColumn= ColumnMin(matrix);
+            matrix = MoveV(MaxColumn, MinColumn, matrix);
+            vSum = VacSum(matrix);
 
-            matrix = MoveV(cMax, cMin, matrix);
-            turn += 1;
-            if(turn == 10){
-                break;
-            }
         }
+        System.out.println();
         for (int[] matrix1 : matrix) {
             System.out.println(Arrays.toString(matrix1));          
         }
+    }
+     
+    public int VacSum(int[][] matrix){
+        List<Integer> list = new ArrayList();
+        int total = 0;
+        int min = matrix[0][0];
+        
+        for(int i = 0; i < 14; i++){
+            total += ColumnSum(matrix,i);
+            if(i == 5 || i == 6 ||i == 12 || i == 13){
+                list.add(total - 5);
+                if(list.get(i) <= min){
+                    min = list.get(i);
+                }
+            }else{
+                list.add(total - 2);
+                if(list.get(i) <= min){
+                    min = list.get(i);
+                }
+            }
+            total = 0;
+        }
+        return min;
     }
 }
